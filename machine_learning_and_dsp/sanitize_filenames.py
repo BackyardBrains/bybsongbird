8c5767e6a23823ab0ad48b91f87b0d78782fa2b5
znrobins <znrobins@umich.edu>
2017-10-31 19:53:24 +0000

    mpfixes

--- machine_learning_and_dsp/sanitize_filenames.py
+++ machine_learning_and_dsp/sanitize_filenames.py
@@ -9,13 +9,13 @@ def sanatize_filenames(directory=unicode(os.getcwd()), verbose=False):
         print "Now sanitizing filenames in root directory: ", directory, '\n'
     for root, dirs, files in os.walk(directory):
         for file in files:
-            if file.endswith('.mp3') or file.endswith('MP3'):
+            if file.endswith('.wav') or file.endswith('WAV'):
                 filename, fileextension = os.path.splitext(file)
-                new_filename = ''.join(e for e in filename if e.isalnum()) + '.mp3'
+                new_filename = ''.join(e for e in filename if e.isalnum()) + '.wav'
                 file = os.path.join(root, file)
                 new_filename = os.path.join(root, new_filename)
                 while (os.path.exists(new_filename)):
-                    new_filename += '1.mp3'
+                    new_filename += '1.wav'
                 if file != new_filename:
                     os.rename(file, new_filename)
                 if verbose:
