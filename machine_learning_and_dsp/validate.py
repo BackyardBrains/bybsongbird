c8b200e1b8bac87b26a9d073231d53fa26ee3752
znrobins <znrobins@umich.edu>
2017-11-01 18:38:21 +0000

    basic validation example script for classifier

--- machine_learning_and_dsp/validate.py
+++ machine_learning_and_dsp/validate.py
@@ -1,3 +1,4 @@
+import itertools
 import os
 from functools import partial
 
@@ -8,7 +9,12 @@ from clean_and_test import clean_and_test, test_params
 from train_model import train_model
 
 
-def train_and_verify(directory, classifierType, birds, mtStep, mtWin, stStep, stWin):
+def train_and_verify(parameters, directory, birds):
+    classifierType = parameters[0]
+    mtStep = parameters[1]
+    mtWin = parameters[2]
+    stStep = parameters[3]
+    stWin = parameters[4]
     train_rootdir = os.path.join(directory, 'Training')
     test_rootdir = os.path.join(directory, 'Testing')
     train_dirs = test_params(train_rootdir, birds)
@@ -16,7 +22,8 @@ def train_and_verify(directory, classifierType, birds, mtStep, mtWin, stStep, st
     train_model(modelName=model, mtWin=mtWin, mtStep=mtStep, stWin=stWin, stStep=stStep, classifierType=classifierType,
                 list_of_dirs=train_dirs)
     clean_and_test(directory=test_rootdir, classifierType=classifierType, no_sanitize=True, skip_clean=True,
-                   show_graphs=False, model_file=model, birds=birds, verbose=False, num_threads=0)
+                   show_graphs=False, model_file=os.path.join(os.getcwd(), model), birds=birds, verbose=False,
+                   num_threads=0)
 
 
 if __name__ == '__main__':
@@ -29,7 +36,9 @@ if __name__ == '__main__':
     stStep = [0.01, 0.02]
     stWin = [0.02, 0.04]
 
-    verifier = partial(target=train_and_verify, directory=directory, birds=birds)
+    parameters = list(itertools.product(classifierType, mtStep, mtWin, stStep, stWin))
+
+    verifier = partial(train_and_verify, directory=directory, birds=birds)
 
     pros = Pool(mp.cpu_count())
-    pros.map(verifier, classifierType, mtStep, mtWin, stStep, stWin)
+    pros.map(verifier, parameters)
