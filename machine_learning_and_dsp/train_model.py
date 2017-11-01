cd76f1e9b2843c762f19163fedd4baa62ef777ba
znrobins <znrobins@umich.edu>
2017-11-01 19:42:27 +0000

    basic validation example script for classifier

--- machine_learning_and_dsp/train_model.py
+++ machine_learning_and_dsp/train_model.py
@@ -8,7 +8,7 @@ def train_model(list_of_dirs, mtStep=0.4, mtWin=0.4, stStep=0.04, stWin=0.04, cl
                 useBeatmap=False):
     # Trains a new classification model see audioTrainTest.py in pyaudioanalysis library for details
     audioTrainTest.featureAndTrain(list_of_dirs, mtWin, mtStep, stWin, stStep, classifierType,
-                                   '_'.join([classifierType, modelName]), useBeatmap)
+                                   modelName, useBeatmap)
 
 
 def train_and_test(list_of_dirs, test_dirs, mtStep=0.4, mtWin=0.4, stStep=0.04, stWin=0.04,
@@ -16,6 +16,6 @@ def train_and_test(list_of_dirs, test_dirs, mtStep=0.4, mtWin=0.4, stStep=0.04, 
                    modelName='gradientboostingTest', useBeatmap=False):
     # automatically trains a new model and then tests it using a seperate dataset, see test_model.py
     train_model(list_of_dirs, mtWin, mtStep, stWin, stStep, classifierType, modelName, useBeatmap)
-    t1 = tester(test_dirs, modelName='_'.join([classifierType, modelName]), classifierType=classifierType)
+    t1 = tester(test_dirs, modelName=modelName, classifierType=classifierType)
     t1.test_model()
     # send_notification('test and train complete') #sms notification; see twillio_test.py
