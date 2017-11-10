6f5be9f55f289f0f047ec01d05d72247e2085244
zhuangjiadi <yuantian@Yuans-MacBook-Pro.local>
2017-11-10 10:38:26 -0500

    improve home page

---
+++ static/js/index.js
@@ -0,0 +1,14 @@
+$(function() {
+    $('.header').on('click', function() {
+        $('.header').each(function() {
+            if ($(this).hasClass('active')) {
+                var desc_no = $(this).attr('data-associated');
+                $('.' + desc_no).hide();
+                $(this).removeClass('active');
+            }
+        })
+        $(this).addClass('active');
+        var desc_no = $(this).attr('data-associated');
+        $('.' + desc_no).show();
+    });
+});
