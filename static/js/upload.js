a087d443143de671bcbe027f139b4d770ae0d592
zhuangjiadi <yuantian@Yuans-MacBook-Pro.local>
2017-11-15 23:28:33 -0500

    rebuild upload page

---
+++ static/js/upload.js
@@ -0,0 +1,29 @@
+$(function() {
+    $('#chooseFile').bind('change', function () {
+        var size = $("#chooseFile")[0].files.length
+        if (size > 1) {
+            $("#noFile").text(size + ' files chosen'); 
+        } else {
+            var filename = $("#chooseFile").val();
+            if (/^\s*$/.test(filename)) {
+                $(".file-upload").removeClass('active');
+                $("#noFile").text("No file chosen..."); 
+            }
+            else {
+                $(".file-upload").addClass('active');
+                $("#noFile").text(filename.replace("C:\\fakepath\\", "")); 
+            }
+        }
+    });
+
+    $('.location_icon').on('click', function() {
+        if (navigator.geolocation) {
+            navigator.geolocation.getCurrentPosition(function(position) {
+                $('.latitude').val(position.coords.latitude);
+                $('.longitude').val(position.coords.longitude);
+            });
+        } else {
+            alert("Geolocation is not supported by this browser.");
+        }
+    });
+});
