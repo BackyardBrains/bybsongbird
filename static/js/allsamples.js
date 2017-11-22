c2d5f27665a0beebb3421faf4f7203b9c64cbcea
wfchou <wfchou@umich.edu>
2017-11-21 21:23:54 -0500

    tests

--- static/js/allsamples.js
+++ static/js/allsamples.js
@@ -1,6 +1,11 @@
 if (sessionStorage.getItem("sort") != null || sessionStorage.getItem("dir") != null){
+    alert("hi");
+    alert(sessionStorage.getItem("sort"));
+    alert(sessionStorage.getItem("dir"));
     document.getElementById("sortid").selectedIndex = sessionStorage.getItem("sort");
     document.getElementById("dirid").selectedIndex = sessionStorage.getItem("dir");
+    alert(document.getElementById("sortid").selectedIndex);
+    alert(document.getElementById("dirid").selectedIndex);
 }
 
 function submitThis() {
