33ed4dae13e0f55fb9df6906d5060d26c40c49f6
wfchou <wfchou@umich.edu>
2017-11-21 21:06:03 -0500

    test

--- static/js/allsamples.js
+++ static/js/allsamples.js
@@ -1,6 +1,10 @@
 document.getElementById("sortid").selectedIndex = sessionStorage.getItem("sort");
 document.getElementById("dirid").selectedIndex = sessionStorage.getItem("dir");
 
+alert("test");
+alert(sessionStorage.getItem("sort"));
+alert(sessionStorage.getItem("dir"));
+
 function submitThis() {
     sessionStorage.setItem("sort", document.getElementById("sortid").selectedIndex);
     sessionStorage.setItem("dir", document.getElementById("dirid").selectedIndex);
