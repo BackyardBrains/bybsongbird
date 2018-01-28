761a1c1063bfb24fd3f4fccedc70fe3de85b382d
cedricIGV <cedrib@umich.edu>
2018-01-28 18:15:10 -0500

    Fixed sound toggle and added image

--- mobileApp/src/App.js
+++ mobileApp/src/App.js
@@ -1,32 +1,40 @@
 import React, {Component} from "react";
-import {View, Text, Image} from "react-native";
+import {View, Text, Image, ScrollView} from "react-native";
 import CardSection from "./components/common/CardSection";
 import Button from "./components/common/Button"
 
+const Sound = require('react-native-sound')
+const s = new Sound('birdinrain.mp3', Sound.MAIN_BUNDLE, (e) => {
+    if (e) {
+      console.log(e);
+    }
+});
+
 class App extends Component{
+  state = {playSound: false};
   playSound() {
-    const Sound = require('react-native-sound')
-    const s = new Sound('birdinrain.mp3', Sound.MAIN_BUNDLE, (e) => {
-            if (e) {
-              console.log('error', e);
-            } else {
-              s.setSpeed(1);
-              console.log('duration', s.getDuration());
-              s.play(() => s.release()); // Release when it's done so we're not using up resources
-            }
-          });
-    console.log("here");
+    if(this.state.playSound == true)
+    {
+      s.stop();
+      console.log("stopping")
+    }
+    if(this.state.playSound == false)
+    {
+      s.play();
+      console.log("playing")
+    }
+    this.setState({playSound: !this.state.playSound})
   }
 
   render()
   {
-    const { imageStyle, viewStyle, textStyle } = styles
+    const { imageStyle, viewStyle, textStyle, headerStyle } = styles
     return(
-      <View>
+      <ScrollView>
         <CardSection>
           <Image
             source={require('./images/byblogo.jpg')}
-            style={imageStyle}
+            style={headerStyle}
           />
         </CardSection>
         <CardSection>
@@ -39,7 +47,24 @@ class App extends Component{
             {"Listen to what's recording right now!"}
           </Button>
         </CardSection>
-      </View>
+
+        <CardSection>
+          <Image
+            source={require("./images/device.jpg")}
+            style={imageStyle}
+          />
+        </CardSection>
+        <View style={viewStyle}>
+          <Text style={textStyle}> The Recording Device </Text>
+        </View>
+        <CardSection>
+          <Text style={textStyle}> Want to upload your own sounds? </Text>
+          <Button>
+            Click Here!
+          </Button>
+        </CardSection>
+
+      </ScrollView>
     );
   }
 }
@@ -49,14 +74,21 @@ const styles = {
     justifyContent: 'center',
     alignItems: 'center',
   },
-  imageStyle: {
+  headerStyle: {
     height: 50,
     flex: 1,
     width: undefined
   },
   textStyle: {
-    fontSize: 18
-  }
+    fontSize: 18,
+    flex:1,
+    paddingLeft: 5
+  },
+  imageStyle: {
+    height: 300,
+    flex: 1,
+    width: undefined
+  },
 }
 
 export default App;
