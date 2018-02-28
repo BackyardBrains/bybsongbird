4b8932b37cabf124846bdf331170016187d16319
egienvalue <juluo@umich.edu>
2018-02-28 12:42:37 -0500

    update readme && regulize code style of firmware

---
+++ firmware/sysdef.h
@@ -0,0 +1,24 @@
+// Sensor system define
+#define DHTTYPE DHT22
+
+// Pin number for peripheral
+#define recordPin 11
+#define sdPin 13
+#define PIN 10
+#define LED_PIN 2
+#define DHTPIN 9
+
+// CPU frequency
+#define CPU_HZ 48000000
+
+#define TIMER_PRESCALER_DIV 64
+#define sampleRate 24000
+#define metaDataSize 92
+#define BUFFER_SIZE 25000
+
+
+
+
+
+
+
