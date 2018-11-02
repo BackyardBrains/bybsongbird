61007c045c14b41d0e8bd152591463790aaa9dd5
c_salad <yfzdq@umich.edu>
2018-11-01 17:19:57 -0700

    jsx model fix

--- js/main.jsx
+++ js/main.jsx
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import InfScroll from './infScroll';
+import Sample from './Sample';
 
 ReactDOM.render(
-  <sample url="/" />,
-  document.getElementById('react-samples'),
+  <Sample url="/" />,
+  document.getElementById("reactEntry"),
 );
