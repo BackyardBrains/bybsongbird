50a20dfe4cc5e9a29a5d5f841878dc4974b54814
c_salad <yfzdq@umich.edu>
2018-11-04 13:19:08 -0800

    added switch to router;

--- controllers/allsamples.py
+++ controllers/allsamples.py
@@ -125,3 +125,136 @@ def allsamples_route():
     }
 
     return render_template("allsamples.html", **options)
+
+
+@allsamples.route('/api/allsamples', methods=['GET', 'POST'])
+def allsamples_api():
+    db = bybsongbird.extensions.connect_to_database()
+    cur = db.cursor()
+
+    result = ''
+
+    search = 'SELECT per1, type1, added, sampleid FROM sampleInfo '
+    good = True
+
+    if request.method == 'POST':
+        button = request.form.get('sort')
+        direction = request.form.get('dir')
+        column = request.form.get('col')
+        equation = request.form.get('equ')
+        match = request.form.get('crit')
+
+        if 'update' in match.lower() or 'delete' in match.lower() or 'insert' in match.lower():
+            good = False
+        elif 'create' in match.lower() or 'alter' in match.lower() or 'drop' in match.lower():
+            good = False
+        elif ';' in match.lower() or 'select' in match.lower():
+            good = False
+
+        if equation != '' and match != '' and good == True:
+            good = False
+
+            search = search + ' WHERE ' + column
+
+            if column == 'type1':
+                if equation == 'equal':
+                    search = search + " LIKE '%" + match + "%' "
+                elif equation == 'notequal':
+                    search = search + " NOT LIKE '%" + match + "%' "
+                elif equation == 'greater':
+                    search = search + " > '" + match + "' "
+                elif equation == 'lesser':
+                    search = search + " < '" + match + "' "
+                elif equation == 'greaterand':
+                    search = search + " >= '" + match + "' "
+                elif equation == 'lesserand':
+                    search = search + " <= '" + match + "' "
+                good = True
+            else:
+                if column == 'added' and match.isdigit() and len(match) == 8:
+                    if equation == 'equal':
+                        search += ' = '
+                    elif equation == 'notequal':
+                        search += ' <> '
+                    elif equation == 'greater':
+                        search += ' > '
+                    elif equation == 'lesser':
+                        search += ' < '
+                    elif equation == 'greaterand':
+                        search += ' >= '
+                    elif equation == 'lesserand':
+                        search += ' <= '
+                    month = match[0:2]
+                    day = match[2:4]
+                    year = match[4:]
+                    if int(month) > 0 and int(month) < 13 and int(day) > 0 and int(day) < 32:
+                        search = search + " '" + year + "-" + month + "-" + day + " 00:00:00'"
+                        good = True
+                    else:
+                        good = False
+                else:
+                    try:
+                        match = float(match)
+                        match = str(match)
+                        if column == 'per1':
+                            if match > 1: match = match / 100.0
+                            if equation == 'equal':
+                                search = search.split("where")[0] + " where abs(per1 - " + match + ") < 0.01"
+                            elif equation == 'notequal':
+                                search = search.split("where")[0] + " where abs(per1 - " + match + ") > 0.01"
+                            else:
+                                search += match
+                        else:
+                            search += match
+                        good = True
+                    except:
+                        good = False
+
+        search = search + ' ORDER BY ' + button
+
+        if direction == 'descending':
+            search += ' DESC'
+
+    if good:
+        cur.execute(search)
+        result = cur.fetchall()
+
+    results = []
+
+    for row in result:
+        birdType = row['type1'][0:row['type1'].find('_')]
+        if 'robin' in birdType:
+            color = 'green'
+        elif 'gold' in birdType:
+            color = 'orange'
+        elif 'jay' in birdType:
+            color = 'lightgr'
+        elif 'chicadee' in birdType:
+            color = 'red'
+        elif 'crow' in birdType:
+            color = 'lightbl'
+        elif 'titmouse' in birdType:
+            color = 'indigo'
+        elif 'cardinal' in birdType:
+            color = 'purple'
+        elif 'sparrow' in birdType:
+            color = 'brown'
+        else:
+            color = 'blue'
+
+        sample = ({
+            "per": round(row['per1'] * 100, 2),
+            "perR": int(round(row['per1'] * 100, 0)),
+            "type": birdType.title(),
+            "date": row['added'].strftime("%b %d %Y"),
+            "wave": os.path.join(config.env['UPLOAD_FOLDER'], 'users_clean/' + str(row['sampleid']) + '.png'),
+            "id": str(row['sampleid']),
+            "color": color
+        })
+        results.append(sample)
+
+    context = {
+        "results": results
+    }
+
+    return jsonify(context)
--- js/main.jsx
+++ js/main.jsx
@@ -1,16 +1,28 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Sample from './Sample';
-import {BrowserRouter as Router, Route} from "react-router-dom";
+import allSamples from './allSamples';
+import {BrowserRouter as Router, Route, Switch} from "react-router-dom";
 
+// render starts with a router, each route matches an url
 ReactDOM.render(
   (
     <Router>
-      <Route path="/" component={sample}/>
+      <Switch>
+        <Route path="/" component={index_samples}/>
+        <Route path="/allsamples" component={all_samples}/>
+      </Switch>
     </Router>
   ),
   document.getElementById("reactEntry"),);
 
-function sample(){
+// render the react components for displaying sample birds on home page
+function index_samples(){
   return (<Sample url="/api/index"/>);
+}
+
+
+// renders the all samples' page react components
+function all_samples(){
+  return (<allSamples url="/api/allsamples"/>);
 }
